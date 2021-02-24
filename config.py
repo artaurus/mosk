cb43bcdcf6a7588300a2a21b0cce85dc15740024
Rishabh Ballal <rishabhballal@gmail.com>
2021-02-24 18:01:35 +0530

    temp

--- config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    SECRET_KEY = '43a7b1952644a1bce603a93c30abc4d6'
-    MONGODB_SETTINGS = {
-        'host': 'mongodb+srv://rishabh:theGunner13@mosk-cluster.rasta.mongodb.net/mongoengine_db?retryWrites=true&w=majority'
-    }
