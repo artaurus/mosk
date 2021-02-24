b28a4fe747e6eae060b0c024bf0b22a5fb5436ab
Rishabh Ballal <rishabhballal@gmail.com>
2021-02-24 10:23:33 +0530

    added create-user view

--- run.py
+++ run.py
@@ -1,58 +1,77 @@
 from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
+from flask_mongoengine import MongoEngine
+from flask_mongoengine.wtf import model_form
 import json
 
+# convert from module to package to app factory
+
 app = Flask(__name__)
+
+# set up config file
+
 with open('secrets.json', 'r') as file:
     secrets = json.loads(file.read())
     app.config['SECRET_KEY'] = secrets['SECRET_KEY']
-    app.config['MONGO_URI'] = secrets['MONGO_URI']
-mongo = PyMongo(app)
+    app.config['MONGODB_SETTINGS'] = {'host': secrets['MONGO_URI']}
+db = MongoEngine(app)
+
+# shift to models.py
+
+class User(db.Document):
+    name = db.StringField(required=True, min_length=4, max_length=20)
+    email = db.EmailField(required=True, min_length=4, max_length=40)
+    meta = {'collection': 'users'}
+
+UserForm = model_form(User)
 
 @app.route('/')
 def home():
-    users = mongo.db.users.find()
-    return render_template('home.html', users=users)
+    return render_template('home.html', users=User.objects)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
-    if request.method == 'POST':
+    form = UserForm(request.form)
+    if request.method == 'POST' and form.validate():
         name = request.form.get('name')
         email = request.form.get('email')
-        mongo.db.users.insert_one({
-            'name': name,
-            'email': email
-        })
+        try:
+            User(name=name, email=email).save()
+        except:
+            pass
         return redirect(url_for('home'))
-    return render_template('add.html')
+    return render_template('add.html', form=form)
 
-@app.route('/remove', methods=['GET', 'POST'])
-def remove():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        mongo.db.users.delete_one({'email': email})
-        return redirect(url_for('home'))
-    return render_template('remove.html')
+# finish crud routes
 
-@app.route('/modify', methods=['GET', 'POST'])
-def modify():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        user = mongo.db.users.find_one({'email': email})
-        if user:
-            return redirect(url_for('modify_user', user=user['name']))
-        else:
-            return redirect(url_for('home'))
-    return render_template('modify.html')
+# @app.route('/remove', methods=['GET', 'POST'])
+# def remove():
+#     if request.method == 'POST':
+#         email = request.form.get('email')
+#         try:
+#             User.objects(email=email).delete()
+#         except:
+#             pass
+#         return redirect(url_for('home'))
+#     return render_template('remove.html')
 
-@app.route('/modify/<string:user>', methods=['GET', 'POST'])
-def modify_user(user):
-    if request.method == 'POST':
-        name = request.form.get('name')
-        mongo.db.users.update_one({'name': user},
-        {'$set': {'name': name}})
-        return redirect(url_for('home'))
-    return render_template('modify_user.html', user=user)
+# @app.route('/modify', methods=['GET', 'POST'])
+# def modify():
+#     if request.method == 'POST':
+#         email = request.form.get('email')
+#
+#         if user:
+#             return redirect(url_for('modify_user', user=user['name']))
+#         else:
+#             return redirect(url_for('home'))
+#     return render_template('modify.html')
+
+# @app.route('/modify/<string:user>', methods=['GET', 'POST'])
+# def modify_user(user):
+#     if request.method == 'POST':
+#         name = request.form.get('name')
+#
+#         return redirect(url_for('home'))
+#     return render_template('modify_user.html', user=user)
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
