be0e58339ae3de555e8d47cb7b0f0c10e9afc22d
Rishabh Ballal <rishabhballal@gmail.com>
2021-02-24 17:40:12 +0530

    added modify-user route

--- run.py
+++ run.py
@@ -1,28 +1,20 @@
 from flask import Flask, render_template, redirect, request, url_for
 from flask_mongoengine import MongoEngine
 from flask_mongoengine.wtf import model_form
-import json
+from config import Config
+from models import User
+
+from flask import jsonify
 
 # convert from module to package
 # convert from package to app factory
 
+db = MongoEngine()
+
 app = Flask(__name__)
+app.config.from_object(Config)
 
-# set up config file
-
-with open('secrets.json', 'r') as file:
-    secrets = json.loads(file.read())
-    app.config['SECRET_KEY'] = secrets['SECRET_KEY']
-    app.config['MONGODB_SETTINGS'] = {'host': secrets['MONGO_URI']}
-db = MongoEngine(app)
-
-# shift to models.py
-
-class User(db.Document):
-    name = db.StringField(required=True, min_length=4, max_length=20)
-    email = db.EmailField(required=True, min_length=4, max_length=40)
-    meta = {'collection': 'users'}
-
+db.init_app(app)
 UserForm = model_form(User)
 
 @app.route('/')
@@ -33,13 +25,11 @@ def home():
 def add():
     form = UserForm(request.form)
     if request.method == 'POST' and form.validate():
-        name = request.form.get('name')
-        email = request.form.get('email')
         try:
-            User(name=name, email=email).save()
+            User(name=form.name, email=form.email).save()
+            return redirect(url_for('home'))
         except:
-            pass
-        return redirect(url_for('home'))
+            return redirect(url_for('add'))
     return render_template('add.html', form=form)
 
 @app.route('/remove', methods=['GET', 'POST'])
@@ -47,32 +37,34 @@ def remove():
     if request.method == 'POST':
         email = request.form.get('email')
         try:
-            User.objects(email=email).delete()
+            User.objects(email=email).first().delete()
+            return redirect(url_for('home'))
         except:
-            pass
-        return redirect(url_for('home'))
+            return redirect(url_for('remove'))
     return render_template('remove.html')
 
-# finish crud routes
+# reduce modify algorithm to one view
 
 @app.route('/modify', methods=['GET', 'POST'])
 def modify():
     if request.method == 'POST':
         email = request.form.get('email')
-
+        user = User.objects(email=email).first()
         if user:
-            return redirect(url_for('modify_user', user=user['name']))
+            return redirect(url_for('modify_user', user=user.name))
         else:
-            return redirect(url_for('home'))
+            return redirect(url_for('modify'))
     return render_template('modify.html')
 
-# @app.route('/modify/<string:user>', methods=['GET', 'POST'])
-# def modify_user(user):
-#     if request.method == 'POST':
-#         name = request.form.get('name')
-#
-#         return redirect(url_for('home'))
-#     return render_template('modify_user.html', user=user)
+@app.route('/modify/<string:user>', methods=['GET', 'POST'])
+def modify_user(user):
+    if request.method == 'POST':
+        name = request.form.get('name')
+        user = User.objects(name=user).first()
+        user.name = name
+        user.save()
+        return redirect(url_for('home'))
+    return render_template('modify_user.html', user=user)
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
