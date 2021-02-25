0497a593405a485c0bf0af8b14f7bba97a4441fa
Rishabh Ballal <rishabhballal@gmail.com>
2021-02-25 12:01:14 +0530

    added modify-user view

--- run.py
+++ run.py
@@ -4,16 +4,9 @@ from flask_mongoengine.wtf import model_form
 from config import Config
 from models import User
 
-from flask import jsonify
-
-# convert from module to package
-# convert from package to app factory
-
 db = MongoEngine()
-
 app = Flask(__name__)
 app.config.from_object(Config)
-
 db.init_app(app)
 UserForm = model_form(User)
 
@@ -25,11 +18,8 @@ def home():
 def add():
     form = UserForm(request.form)
     if request.method == 'POST' and form.validate():
-        try:
-            User(name=form.name, email=form.email).save()
-            return redirect(url_for('home'))
-        except:
-            return redirect(url_for('add'))
+        User(name=form.name.data, email=form.email.data).save()
+        return redirect(url_for('home'))
     return render_template('add.html', form=form)
 
 @app.route('/remove', methods=['GET', 'POST'])
@@ -43,28 +33,15 @@ def remove():
             return redirect(url_for('remove'))
     return render_template('remove.html')
 
-# reduce modify algorithm to one view
-
 @app.route('/modify', methods=['GET', 'POST'])
 def modify():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        user = User.objects(email=email).first()
-        if user:
-            return redirect(url_for('modify_user', user=user.name))
-        else:
-            return redirect(url_for('modify'))
-    return render_template('modify.html')
-
-@app.route('/modify/<string:user>', methods=['GET', 'POST'])
-def modify_user(user):
-    if request.method == 'POST':
-        name = request.form.get('name')
-        user = User.objects(name=user).first()
-        user.name = name
+    form = UserForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User.objects(email=form.email.data).first()
+        user.name = form.name.data
         user.save()
         return redirect(url_for('home'))
-    return render_template('modify_user.html', user=user)
+    return render_template('modify.html', form=form)
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
