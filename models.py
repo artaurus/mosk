6437f555f553f5a0c08f871963eb3e074bfd6842
Rishabh Ballal <rishabhballal@gmail.com>
2021-02-27 19:11:26 +0530

    added login and modify-user forms

--- models.py
+++ models.py
@@ -3,5 +3,15 @@ from mongoengine.fields import StringField, EmailField
 
 class User(Document):
     name = StringField(required=True, min_length=3, max_length=20)
-    email = EmailField(required=True, min_length=3, max_length=40)
+    email = EmailField(required=True, unique=True, min_length=3, max_length=40)
+    # requires js max_length validation
+    password = StringField(required=True, min_length=3, max_length=100)
     meta = {'collection': 'users'}
+
+class Login(Document):
+    email = EmailField(required=True, min_length=3, max_length=40)
+    password = StringField(required=True, min_length=3, max_length=20)
+
+class ModifyProfile(Document):
+    name = StringField(required=True, min_length=3, max_length=20)
+    email = EmailField(required=True, unique=True, min_length=3, max_length=40)
