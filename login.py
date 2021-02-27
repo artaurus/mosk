c5fb4f0fc60bd4d2fd8bc9fe229e04809d84c62f
Rishabh Ballal <rishabhballal@gmail.com>
2021-02-27 18:47:55 +0530

    added view decorators

--- login.py
+++ login.py
@@ -1,31 +1,40 @@
-from functools import wraps
 from flask import redirect, request, url_for
 
 class LoginHandler:
     def __init__(self):
         self.reset_user()
 
+    def set_user(self, user):
+        if not self.status():
+            del user.password
+            self.user = user
+            with open('static/user.json', 'w') as file:
+                file.write(user.to_json())
+
+    def reset_user(self):
+        self.user = {}
+        with open('static/user.json', 'w') as file:
+            file.write('{}')
+
     def get_user(self):
         return self.user
 
-    def set_user(self, name, email):
-        self.log = True
-        self.user = {
-            'name': name,
-            'email': email,
-        }
+    def status(self):
+        return bool(self.user)
 
-    def reset_user(self):
-        self.log = False
-        self.user = {
-            'name': '',
-            'email': '',
-        }
-
-    def login_required(self, func):
-        @wraps(func)
+    def access_required(self, func):
         def wrapper(*args, **kwargs):
-            if not self.log:
-                return redirect(url_for('login', next=request.url))
+            if not self.status():
+                return redirect(url_for('login', next=url_for(func.__name__)[1:]))
             return func(*args, **kwargs)
+        wrapper.__name__ = func.__name__
+        return wrapper
+
+    def access_denied(self, func):
+        def wrapper(*args, **kwargs):
+            if self.status():
+                # create custom error pages
+                return redirect(url_for('home'))
+            return func(*args, **kwargs)
+        wrapper.__name__ = func.__name__
         return wrapper
