604f519684c29c46a75d2b12008d0633dda2904e
Rishabh Ballal <rishabhballal@gmail.com>
2021-03-04 10:32:19 +0530

    app factory construct

--- mosk/__init__.py
+++ mosk/__init__.py
@@ -1,15 +1,22 @@
 from flask import Flask
 from flask_mongoengine import MongoEngine
-from mosk.config import Config
-from mosk.login import LoginHandler
+from .config import Config
+from .login import LoginHandler
 
 db = MongoEngine()
 log = LoginHandler()
 
-app = Flask(__name__)
-app.config.from_object(Config)
+def init_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-db.init_app(app)
-log.init_app(app)
+    db.init_app(app)
+    log.init_app(app)
 
-from mosk import views
+    with app.app_context():
+        from mosk.gen import gen
+        from mosk.users import users
+        app.register_blueprint(gen)
+        app.register_blueprint(users)
+
+        return app
