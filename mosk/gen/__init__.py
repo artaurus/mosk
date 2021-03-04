7be16b0ac109c212b84acdf19a95c8a98abbf839
Rishabh Ballal <rishabhballal@gmail.com>
2021-03-04 12:07:57 +0530

    add: static and template paths

--- mosk/gen/__init__.py
+++ mosk/gen/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-gen = Blueprint('gen', __name__)
+gen = Blueprint('gen', __name__, template_folder='templates', static_folder='static', static_url_path='/gen/static')
 
 from . import views
