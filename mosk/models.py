dd1940b886b885f611b88afa8cecb36799478208
Rishabh Ballal <rishabhballal@gmail.com>
2021-03-06 12:39:34 +0530

    fix: name length validation

--- mosk/models.py
+++ mosk/models.py
@@ -1,7 +1,7 @@
 from mosk import db
 
 class User(db.Document):
-    name = db.StringField(required=True, min_length=3, max_length=20)
-    email = db.EmailField(required=True, unique=True, min_length=3, max_length=40)
+    name = db.StringField(required=True, min_length=3, max_length=25)
+    email = db.EmailField(required=True, min_length=3, max_length=40, unique=True)
     password = db.StringField(required=True, min_length=3, max_length=100)
     meta = {'collection': 'users'}
