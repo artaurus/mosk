fc89a2dbd511df515c0eed505da706cba4b3c2b2
Rishabh Ballal <rishabhballal@gmail.com>
2021-03-08 18:42:55 +0530

    add: filter users via search

--- mosk/gen/views.py
+++ mosk/gen/views.py
@@ -6,5 +6,9 @@ from . import gen
 @gen.route('/')
 def home():
     page = request.args.get('page', 1, type=int)
-    users = User.objects.paginate(page=page, per_page=8)
+    search = request.args.get('search')
+    if search:
+        users = User.objects.search_text(search).paginate(page=page, per_page=8)
+    else:
+        users = User.objects.paginate(page=page, per_page=8)
     return render_template('home.html', users=users, log=log)
