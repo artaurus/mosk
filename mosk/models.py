adfb44cba0acd77ce71616196ca00fcc29bb1ce7
Rishabh Ballal <rishabhballal@gmail.com>
2021-03-08 18:25:12 +0530

    add: db indexes

--- mosk/models.py
+++ mosk/models.py
@@ -4,4 +4,10 @@ class User(db.Document):
     name = db.StringField(required=True, min_length=3, max_length=25)
     email = db.EmailField(required=True, min_length=3, max_length=40, unique=True)
     password = db.StringField(required=True, min_length=3, max_length=100)
-    meta = {'collection': 'users'}
+
+    meta = {'collection': 'users',
+            'indexes': [{'fields': ['$name', '$email'],
+                        'default_language': 'english',
+                        'weights': {'name':10, 'email':4}
+                        }]
+            }
