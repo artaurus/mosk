f0d0c98923b725b0a1931fdb91e476b54d90268f
Rishabh Ballal <rishabhballal@gmail.com>
2021-03-08 12:04:28 +0530

    add: user pagination

--- mosk/gen/views.py
+++ mosk/gen/views.py
@@ -1,8 +1,10 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, request
 from .. import log
 from ..models import User
 from . import gen
 
 @gen.route('/')
 def home():
-    return render_template('home.html', users=User.objects, log=log)
+    page = request.args.get('page', 1, type=int)
+    users = User.objects.paginate(page=page, per_page=8)
+    return render_template('home.html', users=users, log=log)
