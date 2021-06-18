bac7cdc911829ff9873c76ea0e173d326d3e27d6
Rishabh Ballal <rishabhballal@gmail.com>
2021-06-18 15:10:48 +0530

    mod: user manager

---
+++ mosk/manager.py
@@ -0,0 +1,43 @@
+from flask import redirect, url_for
+
+class UserManager:
+    def __init__(self, app):
+        self._path = app.root_path + app.config['PATH_TO_USER_JSON']
+        self.reset_user()
+
+    def reset_user(self):
+        self._user = {}
+        with open(self._path, 'w') as file:
+            file.write('{}')
+
+    def set_user(self, user):
+        if not self.status():
+            del user.id
+            del user.password
+            self._user = user
+            with open(self._path, 'w') as file:
+                file.write(user.to_json())
+
+    def get_user(self, attr=None):
+        if attr:
+            return self._user[attr]
+        return self._user
+
+    def status(self):
+        return bool(self._user)
+
+    def access_required(self, func):
+        def wrapper(*args, **kwargs):
+            if not self.status():
+                return redirect(url_for('users.login'))
+            return func(*args, **kwargs)
+        wrapper.__name__ = func.__name__
+        return wrapper
+
+    def access_restricted(self, func):
+        def wrapper(*args, **kwargs):
+            if self.status():
+                return redirect(url_for('gen.home'))
+            return func(*args, **kwargs)
+        wrapper.__name__ = func.__name__
+        return wrapper
