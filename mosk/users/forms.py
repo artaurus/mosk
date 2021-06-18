bcdc0a0c4eb744c9fad40d46996e15871b3a7288
Rishabh Ballal <rishabhballal@gmail.com>
2021-06-18 15:28:22 +0530

    add: custom validators, password reset form

--- mosk/users/forms.py
+++ mosk/users/forms.py
@@ -1,30 +1,38 @@
 from wtforms import Form, StringField, PasswordField
-from wtforms.validators import InputRequired, Length, Email, Regexp, EqualTo, ValidationError
-from ..models import User
-from .. import log
+from wtforms.validators import Length, Email, Regexp, EqualTo, ValidationError
+from mosk import um
+from mosk.users.models import User
 
-class Unique:
-    def __init__(self, model, message=None):
-        self.model = model
-        self.message = message if message else 'Already exists in the database.'
+def input_required(form, field):
+    if len(field.data) == 0:
+        raise ValidationError('')
 
-    def __call__(self, form, field):
-        if self.model.objects(__raw__={field.name: field.data}).first():
-            if log.status() and field.data == log.get_user()['email']:
-                pass
-            else:
-                raise ValidationError(self.message)
+def unique(form, field):
+    if User.objects(email=field.data).first():
+        if um.status() and field.data == um.get_user('email'):
+            pass
+        else:
+            raise ValidationError('Already exists in our database.')
+
+def not_registered(form, field):
+    if not User.objects(email=field.data).first():
+        raise ValidationError('Does not exist in our database.')
 
 class SignUpForm(Form):
-    name = StringField(label='Name', validators=[InputRequired(), Length(min=3, max=25), Regexp('^[a-zA-Z]+( ?-?[a-zA-Z]+)*$')])
-    email = StringField(label='Email', validators=[Email(), InputRequired(), Length(min=3, max=40), Unique(User)])
-    password = PasswordField(label='Password', validators=[InputRequired(), Length(min=3, max=20), Regexp('^[a-zA-Z0-9_]+$')])
+    email = StringField(label='Email address', validators=[input_required, unique, Length(min=5, max=254), Regexp('^[a-zA-Z0-9_\-\.]+@[a-zA-Z0-9\-\.]+\.[a-zA-Z]+$')])
+    password = PasswordField(label='Password', validators=[input_required, Length(min=8, max=20), Regexp('^[a-zA-Z0-9_]+$')])
     confirm_password = PasswordField(label='Confirm password', validators=[EqualTo('password')])
 
 class LoginForm(Form):
-    email = StringField(label='Email', validators=[Email(), InputRequired(), Length(min=3, max=40)])
-    password = PasswordField(label='Password', validators=[InputRequired(), Length(min=3, max=20), Regexp('^[a-zA-Z0-9_]+$')])
+    email = StringField(label='Email address', validators=[input_required, not_registered, Length(min=5, max=254), Regexp('^[a-zA-Z0-9_\-\.]+@[a-zA-Z0-9\-\.]+\.[a-zA-Z]+$')])
+    password = PasswordField(label='Password', validators=[input_required, Length(min=8, max=20), Regexp('^[a-zA-Z0-9_]+$')])
 
-class ModifyProfileForm(Form):
-    name = StringField(label='Name', validators=[InputRequired(), Length(min=3, max=25), Regexp('^[a-zA-Z]+( ?-?[a-zA-Z]+)*$')])
-    email = StringField(label='Email', validators=[Email(), InputRequired(), Length(min=3, max=40), Unique(User)])
+class EditProfileForm(Form):
+    email = StringField(label='Email address', validators=[input_required, unique, Length(min=5, max=254), Regexp('^[a-zA-Z0-9_\-\.]+@[a-zA-Z0-9\-\.]+\.[a-zA-Z]+$')])
+
+class EmailForm(Form):
+    email = StringField(label='Email address', validators=[input_required, not_registered, Length(min=5, max=254), Regexp('^[a-zA-Z0-9_\-\.]+@[a-zA-Z0-9\-\.]+\.[a-zA-Z]+$')])
+
+class ResetPasswordForm(Form):
+    password = PasswordField(label='New password', validators=[input_required, Length(min=8, max=20), Regexp('^[a-zA-Z0-9_]+$')])
+    confirm_password = PasswordField(label='Confirm password', validators=[EqualTo('password')])
