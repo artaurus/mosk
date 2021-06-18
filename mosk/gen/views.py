2c9ed546548b1ff0d89f3cf1e46413d914ec0d29
Rishabh Ballal <rishabhballal@gmail.com>
2021-06-18 15:26:19 +0530

    del: user content

--- mosk/gen/views.py
+++ mosk/gen/views.py
@@ -1,14 +1,10 @@
 from flask import Flask, Blueprint, render_template, request
-from .. import log
-from ..models import User
-from . import gen
+from mosk import um
+from mosk.gen import gen
 
 @gen.route('/')
 def home():
-    page = request.args.get('page', 1, type=int)
-    search = request.args.get('search')
-    if search:
-        users = User.objects.search_text(search).paginate(page=page, per_page=8)
-    else:
-        users = User.objects.paginate(page=page, per_page=8)
-    return render_template('home.html', users=users, log=log)
+    return render_template(
+        '_base.html',
+        um=um
+    )
