91553b737e499215cdf591c24a705c0bf09d2583
Rishabh Ballal <rishabhballal@gmail.com>
2021-06-27 17:48:27 +0530

    add: static

--- mosk/errors/__init__.py
+++ mosk/errors/__init__.py
@@ -3,7 +3,9 @@ from flask import Blueprint
 errors = Blueprint(
     'errors',
     __name__,
-    template_folder='templates'
+    template_folder='templates',
+    static_folder='static',
+    static_url_path='/errors/static',
 )
 
 from mosk.errors import handler
